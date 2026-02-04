7cdabe63052f9ddb48547fc07636a42599a9e62c
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2026-02-03 22:38:31 -0500

    third commit 04.02.2026

--- lib/GENERATE_PDF_INVOICE/api/pdf_invoice_api.dart
+++ lib/GENERATE_PDF_INVOICE/api/pdf_invoice_api.dart
@@ -5,6 +5,7 @@ import 'package:flutterapp_zorinos/GENERATE_PDF_INVOICE/api/pdf_api.dart';
 import 'package:flutterapp_zorinos/GENERATE_PDF_INVOICE/model/customer.dart';
 import 'package:flutterapp_zorinos/GENERATE_PDF_INVOICE/model/invoice.dart';
 import 'package:flutterapp_zorinos/GENERATE_PDF_INVOICE/model/supplier.dart';
+import 'package:flutterapp_zorinos/GENERATE_PDF_INVOICE/utils.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 
@@ -65,11 +66,93 @@ class PdfInvoiceApi {
     );
   }
 
-  static buildInvoice(Invoice invoice) {}
+  static buildInvoice(Invoice invoice) {
 
-  static buildTitle(Invoice invoice) {}
+    final headers =[
+      'Description',
+      'Date',
+      'Quantity',
+      'Unit Price',
+      'Vat',
+      'Total'
+    ];
 
-  static buildTotal(Invoice invoice) {}
+    final data = invoice.items.map((item){
+
+      final total= item.unitPrice*item.quantity*(1+item.vat);
+
+      return [
+        item.description,
+        Utils.formatDate(item.date),
+        '${item.quantity}',
+        '\$ ${item.unitPrice}',
+        '${item.vat} %',
+        '\$ ${total.toStringAsFixed(2)}'
+      ];
+
+    }).toList();
+
+    return TableHelper.fromTextArray(data: data,headers: headers,
+    border: null,
+    headerStyle: TextStyle(fontWeight: FontWeight.bold),
+    headerDecoration:  BoxDecoration(color: PdfColors.grey300),
+    cellHeight: 30,
+    cellAlignments: {
+      0: Alignment.centerLeft,
+      1: Alignment.centerRight,
+      2: Alignment.centerRight,
+      3:Alignment.centerRight,
+      4:Alignment.centerRight,
+      5:Alignment.centerRight
+    });
+  }
+
+  static buildTitle(Invoice invoice) {
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('INVOICE',
+        style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)),
+        SizedBox(height: 0.8*PdfPageFormat.cm),
+        Text(invoice.info.description),
+        SizedBox(height: 0.8*PdfPageFormat.cm)
+      ]
+    );
+  }
+
+  static buildTotal(Invoice invoice) {
+    final netTotal = invoice.items.map((item)=>item.unitPrice*item.quantity)
+        .reduce((item1,item2)=>item1+item2);
+    final vatPercent = invoice.items.first.vat;
+    final vat = netTotal*vatPercent;
+    final total = netTotal*vat;
+    return Container(
+      alignment:  Alignment.centerRight,
+      child: Row(
+        children: [
+          Spacer(flex: 6),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildText(title: 'Net total', value: Utils.formatPrice(vat),
+              unite: true),
+              Divider(),
+              buildText(title: 'Total amount due',titleStyle: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold
+              ), value:Utils.formatPrice(total),
+              unite: true),
+              SizedBox(height:  2*PdfPageFormat.mm),
+              Container(height: 1,color: PdfColors.grey400),
+              SizedBox(height: 0.5*PdfPageFormat.mm),
+              Container(height: 1,color: PdfColors.grey400)
+            ]
+          ),flex: 4)
+        ]
+      )
+    );
+  }
   static buildFooter(Invoice invoice) {}
 
   static Widget buildCustomerAddress(Customer customer)
@@ -80,13 +163,41 @@ class PdfInvoiceApi {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
 
+
+        Text(customer.name,style: TextStyle(fontWeight: FontWeight.bold)),
+        Text(customer.address)
+
       ]
 
     );
   }
 
   static Widget buildInvoiceInfo(InvoiceInfo info){
-    return Column();
+    final paymentTerms = '${info.dueDate.difference(info.date).inDays} days';
+    final titles =<String>[
+      'Invoice Number:',
+      'Invoice Date:',
+      'Payment Terms:',
+      'Due Date:'
+    ];
+
+    final data = <String>[
+      info.number,
+      Utils.formatDate(info.date),
+      paymentTerms,
+      Utils.formatDate(info.dueDate)
+
+    ];
+    return Column(
+
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List.generate(titles.length, (index){
+        final title = titles[index];
+        final value = data[index];
+
+        return buildText(title: title, value: value,width: 200);
+      })
+    );
   }
   static Widget buildSupplierAddress(Supplier supplier)
 
@@ -102,5 +213,25 @@ class PdfInvoiceApi {
        Text(supplier.address)
      ],
    );
+  }
+
+
+  static buildText({required String title,
+  required String value,
+  double width = double.infinity,
+  TextStyle? titleStyle,
+  bool unite = false}) {
+
+    final style = titleStyle??TextStyle(fontWeight: FontWeight.bold);
+    return Container(
+      width: width,
+      child: Row(
+        children: [
+          Expanded(child: Text(title,style: style)),
+          Text(value,style: unite?style:null)
+        ]
+      )
+    );
+
   }
 }
